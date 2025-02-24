b730508abd5df2acb6159eaca560f33f11162b38
Anshuman Raj <84335613+its-Anshuman@users.noreply.github.com>
2025-02-25 02:23:27 +0530

    setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,5 @@ enableCORS = false\n\
 headless = true\n\
 \n\
 " > ~/.streamlit/config.toml
+
+wget --no-check-certificate "https://drive.google.com/uc?export=download&id=1chG_WEVVkWRI5Iv8bsVn7og-WnK-1MiS" -O similarity.pkl
